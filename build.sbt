ee5607661d78cea01ce1c3b5bb72e3ea9a296df1
Regner Blok-Andersen <regner@bench.co>
2017-02-04 12:21:48 -0800

    ad

--- build.sbt
+++ build.sbt
@@ -1,19 +1,20 @@
-name := """test-service"""
+name := """games-service"""
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.11.0"
 
 val http4sVersion = "0.14.6"
 val circeVersion = "0.5.1"
 
 libraryDependencies ++= Seq(
-  "org.slf4j"  %  "slf4j-simple"        % "1.7.21",
-  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
-  "org.http4s" %% "http4s-dsl"          % http4sVersion,
-  "org.http4s" %% "http4s-argonaut"     % http4sVersion,
-  "org.http4s" %% "http4s-circe"        % http4sVersion,
-  "io.circe"   %% "circe-core"          % circeVersion,
-  "io.circe"   %% "circe-generic"       % circeVersion
+  "org.slf4j"         %  "slf4j-simple"        % "1.7.21",
+  "com.h2database"    %  "h2"                  % "1.4.191",
+  "org.http4s"        %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s"        %% "http4s-dsl"          % http4sVersion,
+  "org.http4s"        %% "http4s-argonaut"     % http4sVersion,
+  "org.http4s"        %% "http4s-circe"        % http4sVersion,
+  "io.circe"          %% "circe-core"          % circeVersion,
+  "io.circe"          %% "circe-generic"       % circeVersion,
+  "com.ibm"           %% "couchdb-scala"       % "0.7.2"
 )
-
--- src/main/scala/com/regner/api/Api.scala
+++ src/main/scala/com/regner/api/Api.scala
@@ -1,15 +1,47 @@
 package com.regner.api
 
-import com.regner.api.service.RootService
+import org.http4s.circe._
+import org.http4s.dsl._
+import org.http4s.HttpService
+import io.circe.generic.auto._
+import io.circe.syntax._
 import org.http4s.server.{Server, ServerApp}
 import org.http4s.server.blaze.BlazeBuilder
+import com.ibm.couchdb._
 
 import scalaz.concurrent.Task
 
+
+
 object Api extends ServerApp {
+  case class Game(name: String)
+
+  val typeMapping = TypeMapping(classOf[Game] -> "Game")
+
+  val couch = CouchDb("192.168.99.100", 32772)
+  val dbName = "games"
+  val db = couch.db(dbName, typeMapping)
+
+  val game1 = Game("game one")
+  val game2 = Game("game two")
+
+  couch.dbs.delete(dbName).ignoreError
+  couch.dbs.create(dbName)
+  db.docs.createMany(Seq(game1, game2))
+
+  private val service = HttpService {
+    case GET -> Root =>
+      val results = db.docs.getMany.byTypeUsingTemporaryView[Game].build.query
+
+      Ok("asd")
+
+    case GET -> Root / IntVar(id) / "turns" =>
+      Ok(List("List of turns for the given game...").asJson)
+  }
+
   override def server(args: List[String]): Task[Server] = {
     BlazeBuilder.bindHttp(8080)
-      .mountService(RootService(), "/")
+      .mountService(service, "/games/")
       .start
   }
 }
